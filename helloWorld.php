48fb90c3b5deb750012f2b8962bd094df7e5532c
Sana <sana.rezaei75@gmail.com>
2021-07-03 23:10:12 +0200

    minor update

--- helloWorld.php
+++ helloWorld.php
@@ -2,30 +2,40 @@
 /**
  * Hello World
  *
- * @package     HelloWorld
+ * @package     ProfileSearch
  * @author      Sana REZAEI
  *
  * @wordpress-plugin
- * Plugin Name: Hello World
+ * Plugin Name: Profile Search for lepopclub
  * Description: This plugin prints "Hello World" inside an admin page.
  * Text Domain: helloWorld
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-add_shortcode('hello_world','display_hello_world_page');
+add_shortcode('profile_search','display_hello_world_page');
 function display_hello_world_page() {
-    echo 'Hello World!';
-  }
-  function hello_world_admin_menu() {
-    add_menu_page(
-          'Hello World',// page title
-          'Hello World',// menu title
-          'manage_options',// capability
-          'hello-world',// menu slug
-          'display_hello_world_page' // callback function
-      );
-  }
-  add_action('admin_menu', 'hello_world_admin_menu');
+    echo 'Profile Search...';
+    get_search_form();
+    get_users();
+    $blogusers = get_users( 'role=subscriber' );
+    // Array of stdClass objects.
+    foreach ( $blogusers as $user ) {
+      echo '<span>' . esc_html( $user->Name ) . '</span>';
+    }
+}
+function hello_world_admin_menu() {
+  add_menu_page(
+        'Hello World',// page title
+        'Hello World',// menu title
+        'manage_options',// capability
+        'hello-world',// menu slug
+        'display_hello_world_page' // callback function
+    );
+}
+add_action('admin_menu', 'hello_world_admin_menu');
+// add_search_form2() {
+//   get_search_form();
+// }
 
   
